c34300b74ffeeb9422041e3cd51563d77eb254da
Shafeen C <126206716+Shaf0@users.noreply.github.com>
2025-08-17 19:05:41 +1000

    working on color sensor

--- project/lib/grimlockcontroller.py
+++ project/lib/grimlockcontroller.py
@@ -8,61 +8,64 @@ from movement import Move
 from lcd import show_state
 
 
-lh_servo_pwm = PWM(Pin(16))
-rh_servo_pwm = PWM(Pin(15))
-lh_servo = Servo(pwm=lh_servo_pwm)
-rh_servo = Servo(pwm=rh_servo_pwm)
-range_a = PiicoDev_Ultrasonic(id=[1, 0, 0, 0])
-range_b = PiicoDev_Ultrasonic(id=[0, 0, 0, 0])
-movement = Move(lh_servo, rh_servo, debug=True)
-sensor = colorvictim("green", debug=True)
+class Controller:
+    def __init__(self):
+        self.lh_servo_pwm = PWM(Pin(16))
+        self.rh_servo_pwm = PWM(Pin(15))
+        self.lh_servo = Servo(pwm=self.lh_servo_pwm)
+        self.rh_servo = Servo(pwm=self.rh_servo_pwm)
+        self.range_a = PiicoDev_Ultrasonic(id=[1, 0, 0, 0])
+        self.range_b = PiicoDev_Ultrasonic(id=[0, 0, 0, 0])
+        self.movement = Move(self.lh_servo, self.rh_servo, debug=True)
+        self.sensor = colorvictim(debug=True)
 
-def navigation():
-    while True:
-        distancea = range_a.distance_mm
-        distanceb = range_b.distance_mm
-
-        if sensor.greenvictim():
-            show_state("Green dectected")
-            print("Green victim detected!") # helps with debugging
-            movement.stop() # stops when it sees the green AH OH
-            sleep_ms(1000) # let it snooze
-            continue # skips the loop
-            
-        movement.backward()
-        show_state("backward")
-        print("Distance A (Left):", distancea, "mm, Distance B (Right):", distanceb, "mm")
-        if distancea <= 100 and distanceb <= 99:
-            movement.stop()
-            show_state("stop")
-            sleep_ms(600)
-            if distancea > distanceb:
-                movement.left()
-                show_state("left")
-            elif distanceb > distancea:
-                movement.right()
+    def navigation(self):
+        while True:
+            distancea = self.range_a.distance_mm
+            distanceb = self.range_b.distance_mm
+            if self.sensor.greenvictim():
+                show_state("Green detected")
+                print("Green victim detected!") # helps with debugging
+                self.movement.stop() # stops when it sees the green AH OH
+                sleep_ms(1000) # let it snooze
+                continue # skips the loop  
+            self.movement.backward()
+            show_state("backward")
+            print("Distance A (Left):", distancea, "mm, Distance B (Right):", distanceb, "mm")
+            if distancea <= 100 and distanceb <= 99:
+                self.movement.stop()
+                show_state("stop")
+                sleep_ms(600)
+                if distancea > distanceb:
+                    self.movement.left()
+                    show_state("left")
+                elif distanceb > distancea:
+                    self.movement.right()
+                    show_state("right")
+                else:
+                    self.movement.left()
+                    show_state("left")  
+                sleep_ms(600)
+            elif distancea <= 100 and distanceb >= 101:
+                self.movement.stop()
+                show_state("stop")
+                sleep_ms(600)
+                self.movement.right()
                 show_state("right")
-            else:
-                movement.left()
-                show_state("left")  
-            sleep_ms(600)
-        elif distancea <= 100 and distanceb >= 101:
-            movement.stop()
-            show_state("stop")
-            sleep_ms(600)
-            movement.right()
-            show_state("right")
-            sleep_ms(600)
-        elif distancea >= 101 and distanceb <= 99:
-            movement.stop()
-            show_state("stop")
-            sleep_ms(600)
-            movement.left()
-            show_state("left")
-            sleep_ms(600)
-        sleep_ms(100)
+                sleep_ms(600)
+            elif distancea >= 101 and distanceb <= 99:
+                self.movement.stop()
+                show_state("stop")
+                sleep_ms(600)
+                self.movement.left()
+                show_state("left")
+                sleep_ms(600)
+            sleep_ms(100)
 
-navigation()
+if __name__ == "__main__":
+    controller = Controller()
+    controller.navigation()
+            
 
 
 
--- tests/colorunit.py
+++ tests/colorunit.py
@@ -1,11 +1,22 @@
 from colorsensor import colorvictim
 from time import sleep_ms
+from machine import Pin, PWM
+from servo import Servo
+from movement import Move
+
+lh_servo_pwm = PWM(Pin(16))
+rh_servo_pwm = PWM(Pin(15))
+lh_servo = Servo(pwm=lh_servo_pwm)
+rh_servo = Servo(pwm=rh_servo_pwm)
+movement = Move(lh_servo, rh_servo, debug=True)
     
 sensor = colorvictim("green", debug=True)
 
 while True:
     if sensor.greenvictim():
         print("Green detected")
+        movement.stop()
     else:
         print("Not the victim")
+        movement.backward
     sleep_ms(1000)
