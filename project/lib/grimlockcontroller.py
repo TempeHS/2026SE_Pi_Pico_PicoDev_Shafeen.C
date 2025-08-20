707d40150732015e5b9b9b73e5c563515cde147f
Shafeen C <126206716+Shaf0@users.noreply.github.com>
2025-08-20 03:19:42 +0000

    grim

--- project/lib/grimlockcontroller.py
+++ project/lib/grimlockcontroller.py
@@ -7,60 +7,64 @@ from colorsensor import colorvictim
 from movement import Move
 from lcd import show_state
 
-# prolly the setup
-lh_servo_pwm = PWM(Pin(16))
-rh_servo_pwm = PWM(Pin(15))
-lh_servo = Servo(pwm=lh_servo_pwm)
-rh_servo = Servo(pwm=rh_servo_pwm)
-range_a = PiicoDev_Ultrasonic(id=[1, 0, 0, 0])
-range_b = PiicoDev_Ultrasonic(id=[0, 0, 0, 0])
-movement = Move(lh_servo, rh_servo, debug=True)
-sensor = colorvictim(debug=True)  
+class GrimlockController:
+    def __init__(self):
+        # Setup
+        lh_servo_pwm = PWM(Pin(16))
+        rh_servo_pwm = PWM(Pin(15))
+        self.lh_servo = Servo(pwm=lh_servo_pwm)
+        self.rh_servo = Servo(pwm=rh_servo_pwm)
+        self.range_a = PiicoDev_Ultrasonic(id=[1, 0, 0, 0])
+        self.range_b = PiicoDev_Ultrasonic(id=[0, 0, 0, 0])
+        self.movement = Move(self.lh_servo, self.rh_servo, debug=True)
+        self.sensor = colorvictim(debug=True)
 
 # Main loop
-while True:
-    distancea = range_a.distance_mm
-    distanceb = range_b.distance_mm  
-    movement.backward()
-    show_state("backward")
-    print("Distance A (Left):", distancea, "mm, Distance B (Right):", distanceb, "mm")
-    if distancea <= 100 and distanceb <= 99:
-        movement.stop()
-        show_state("stop")
-        sleep_ms(600)
-        if distancea > distanceb:
-            movement.left()
-            show_state("left")
-        elif distanceb > distancea:
-            movement.right()
-            show_state("right")
-        else:
-            movement.left()
-            show_state("left")  
-        sleep_ms(600)
-    elif distancea <= 100 and distanceb >= 101:
-        movement.stop()
-        show_state("stop")
-        sleep_ms(600)
-        movement.right()
-        show_state("right")
-        sleep_ms(600)
-    elif distancea >= 101 and distanceb <= 99:
-        movement.stop()
-        show_state("stop")
-        sleep_ms(600)
-        movement.left()
-        show_state("left")
-        sleep_ms(600)
-    if sensor.greenvictim():
-        show_state("Green detected")
-        print("Green victim detected!")
-        movement.stop()
-        sleep_ms(1000)
-        continue  # skip the rest of the loop if green is detected
-    sleep_ms(100)
+    def run(self):
+        while True:
+            distancea = self.range_a.distance_mm
+            distanceb = self.range_b.distance_mm  
+            self.movement.backward()
+            show_state("backward")
+            print("Distance A (Left):", distancea, "mm, Distance B (Right):", distanceb, "mm")
+            if distancea <= 100 and distanceb <= 99:
+                self.movement.stop()
+                show_state("stop")
+                sleep_ms(600)
+                if distancea > distanceb:
+                    self.movement.left()
+                    show_state("left")
+                elif distanceb > distancea:
+                    self.movement.right()
+                    show_state("right")
+                else:
+                    self.movement.left()
+                    show_state("left")  
+                sleep_ms(600)
+            elif distancea <= 100 and distanceb >= 101:
+                self.movement.stop()
+                show_state("stop")
+                sleep_ms(600)
+                self.movement.right()
+                show_state("right")
+                sleep_ms(600)
+            elif distancea >= 101 and distanceb <= 99:
+                self.movement.stop()
+                show_state("stop")
+                sleep_ms(600)
+                self.movement.left()
+                show_state("left")
+                sleep_ms(600)
+            if self.sensor.greenvictim():
+                show_state("Green detected")
+                print("Green victim detected!")
+                self.movement.stop()
+                sleep_ms(1000)
+                continue  # skip the rest of the loop if green is detected
+            sleep_ms(100)
 
-
+controller = GrimlockController()
+controller.run()
 
 
             
